7ad647ff7218699374e6c0ca324cd1a6b76e3454
Nazgul <guobing_xu@163.com>
2015-07-27 15:25:46 +0800

    这个版本出问题了，echarts没有显示了
    这个版本出问题了，echarts没有显示了

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,9 +3,33 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        $arr = array("1","3","3","2","2","3","9");
+        $arr = array("2015-7-1", "2015-7-2", "2015-7-3", "2015-7-4", "2015-7-5", "2015-7-6",
+                    "2015-7-7", "2015-7-8", "2015-7-9", "2015-7-10", "2015-7-11", "2015-7-12",
+                    "2015-7-13", "2015-7-14", "2015-7-15", "2015-7-16");
+        $arr1 = array("1", "2", "3", "4", "5", "6","1", "2", "3", "4", "5", "6","1", "2", "3", "4");
+        $arr2 = array("3", "4", "5", "6", "1", "2","3", "4", "5", "6", "1", "2","3", "4", "5", "6");
+        $arr3 = array("4", "6", "8", "10", "6", "8","4", "6", "8", "10", "6", "8","4", "6", "8", "10");
+        $json = json_encode($arr);
+        $json1 = json_encode($arr1);
+        $json2 = json_encode($arr2);
+        $json3 = json_encode($arr3);
+        $this->assign('arr',$json);
+        $this->assign('arr1',$json1);
+        $this->assign('arr2',$json2);
+        $this->assign('arr3',$json3);
+        $this->display();
+    }
+
+    public function test(){
+        $arr = array("2015-7-1", "2015-7-2", "2015-7-3", "2015-7-4", "2015-7-5", "2015-7-6",
+            "2015-7-7", "2015-7-8", "2015-7-9", "2015-7-10", "2015-7-11", "2015-7-12",
+            "2015-7-13", "2015-7-14", "2015-7-15", "2015-7-16");
         $json = json_encode($arr);
         $this->assign('arr',$json);
         $this->display();
+    }
+
+    public function ccc(){
+        $this->show("adsf");
     }
 }
